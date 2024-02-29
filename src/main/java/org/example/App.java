a501b4fbc0abeb3bdcd883f85bbb008e3020cee0
krisha7desai <krishad2001@gmail.com>
2024-02-28 20:14:22 -0500

    FinalFile

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,23 +4,34 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main(String[] args) {}
-    public static String fb(int i){
+public class App {
+    public static void main(String[] args) {
+        StringBuilder f = new StringBuilder();
+        for (int z = 1; z <= 100; z++) {
+            String b = fb(z);
+            f.append(b);
+            if (z < 100) {
 
-        if (i% 15 == 0) {
-            return "FizzBuzz";
-        }
-        else if (i% 3 == 0) {
-            return "Fizz";
+                f.append(",");
             }
-        else if (i% 5 == 0) {
-            return "Buzz";
-        }
 
-        else {
+        }
+        System.out.println(f);
+    }
+
+
+    public static String fb(int i) {
+
+        if (i % 15 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
+        } else {
             return Integer.toString(i);
         }
+
+
     }
 }
