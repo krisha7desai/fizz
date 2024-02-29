e4c2ba4f89f8d3f1a299fc397c3f0c9c421b4d08
krisha7desai <krishad2001@gmail.com>
2024-02-28 20:01:02 -0500

    pass for 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,9 @@ public class App
     public static void main(String[] args) {}
     public static String fb(int i){
 
-
+        if (i% 3 == 0) {
+            return "Fizz";
+            }
 
 
         return Integer.toString(i);
